06da274b91d373cea37d2a206aa9a3c908b7600d
Yeki0427 <yueqi0427@gmail.com>
2025-05-02 03:44:28 +0800

    添加微信验证日志

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ app = Flask(__name__)
 @app.route('/wechat', methods=['GET', 'POST'])
 def wechat():
     if request.method == 'GET':
-        token = os.environ.get("TOKEN", "yeki")
         signature = request.args.get('signature')
         timestamp = request.args.get('timestamp')
         nonce = request.args.get('nonce')
         echostr = request.args.get('echostr')
-        if not all([signature, timestamp, nonce, echostr]):
-            return '缺参数'
-
+        token = 'yeki'
         tmp_list = [token, timestamp, nonce]
         tmp_list.sort()
         tmp_str = ''.join(tmp_list)
         hashcode = hashlib.sha1(tmp_str.encode('utf-8')).hexdigest()
-
+      
+        # 调试：打印计算出的签名和微信提供的签名
+        #print(f"计算的签名: {hashcode}")
+        #print(f"微信提供的签名: {signature}")
+      
         print(f"微信签名：{signature}")
         print(f"计算签名：{hashcode}")
         print("== 微信请求进入 ==")
@@ -29,12 +30,14 @@ def wechat():
         print("timestamp:", timestamp)
         print("nonce:", nonce)
         print("echostr:", echostr)
-
-
+        
         if hashcode == signature:
             return echostr
         else:
             return '验证失败'
+
+       
+
     else:
         return '暂未处理 POST'
     
